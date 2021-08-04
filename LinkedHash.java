51fc7c40698ba4c172c6c99f9363f0881d134a44
VMVLakshmi <88193171+VMVLakshmi@users.noreply.github.com>
2021-08-04 19:44:54 +0530

    Add files via upload

---
+++ LinkedHash.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class LinkedHash {
+
+	public static void main(String[] args) {
+		 LinkedHashMap<Integer, String> lhmap = 
+                 new LinkedHashMap<Integer, String>();
+         lhmap.put(22, "Abey");
+         lhmap.put(33, "Dawn");
+         lhmap.put(1, "Sherry");
+         lhmap.put(2, "Karon");
+         lhmap.put(100, "Jim");
+         Set set = lhmap.entrySet();
+         Iterator iterator = set.iterator();
+         while(iterator.hasNext()) {
+            Map.Entry me = (Map.Entry)iterator.next();
+            System.out.print("Key is: "+ me.getKey() + 
+                    "& Value is: "+me.getValue()+"\n");
+         }
+    }
+
+}
