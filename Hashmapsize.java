d41f9ec537cb1a4ea3c503849ac7401bb395b123
VMVLakshmi <88193171+VMVLakshmi@users.noreply.github.com>
2021-08-04 19:42:32 +0530

    Add files via upload

---
+++ Hashmapsize.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Hashmapsize {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
+	    hashmap.put(11,"Value1");
+	    hashmap.put(22,"Value2");
+	    hashmap.put(33,"Value3");
+	    hashmap.put(44,"Value4");
+	    hashmap.put(55,"Value5");
+	    System.out.println("Size of HashMap : " + hashmap.size());
+	 }
+
+	}
+
+
